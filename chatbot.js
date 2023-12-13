44ff3f28000ed826cf17c6079653dcc9e5ba7ce6
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2023-12-13 16:43:12 +0530

    chatbot api updated as it got disabled

--- chatbot.js
+++ chatbot.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
